67e64f5bec3b8d6ceac5eeef261ace337381f999
Kozica15 <37438761+Kozica15@users.noreply.github.com>
2018-10-05 16:03:37 +0200

    Update iledufrioul3.js

--- js/iledufrioul3.js
+++ js/iledufrioul3.js
@@ -1,10 +1,6 @@
 
  
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  <script src="site.js"></script>
-            <script>
+  
                 var appelAjax = function (urlApiAjax, callbackJson) {
                     // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
                     fetch(urlApiAjax)
