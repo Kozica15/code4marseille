be8e3e9c2f89a7243a47568126570bc51fbd2a3a
Kozica15 <37438761+Kozica15@users.noreply.github.com>
2018-10-05 15:18:46 +0200

    Update iledufrioul3.js

--- iledufrioul3.js
+++ iledufrioul3.js
@@ -1,1 +1,48 @@
 
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script src="site.js"></script>
+            <script>
+                var appelAjax = function (urlApiAjax, callbackJson) {
+                    // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+                    fetch(urlApiAjax)
+                        .then(function (data) {
+                            // DEBUG
+                            console.log(data);
+                            // ON VEUT RECEVOIR UN OBJET JAVASCRIPT
+                            return data.json();
+                        })
+                        .then(callbackJson)
+                }
+                // URL API AJAX
+                var urlApiAjax = 'https://myprovence.code4marseille.fr/api/instas?itemsPerPage=24';
+                var ajouterImage = function (objetJS) {
+                    console.log(objetJS);
+                    // CA Y'EST J'AI UN OBJET JS AVEC TOUTES INFOS PLANQUEES DEDANS...
+                    // IL FAUT ALLER RECUPERER LES INFOS QUI NOUS INTERESSENT
+                    var tableauInfo = objetJS["hydra:member"];
+                    // objet.propriete OU objet["propriete"]
+                    // BOUCLE POUR PARCOURIR LES INFOS UNE PAR UNE
+                    for (var index = 0; index < tableauInfo.length; index++) {
+                        var infoCourante = tableauInfo[index];
+                        console.log(infoCourante);
+                        var link = infoCourante.link;
+                        var thumbnail = infoCourante.thumbnail;
+                        var lowResolution = infoCourante.lowResolution;
+                        var standardResolution = infoCourante.standardResolution;
+                        if (link) {
+                            var baliseUl = document.querySelector("div.listePhoto");
+                            // DOM Document Object Model
+                            // AJOUTER UNE BALISE li
+                            var codeHtmlLi ='<a href="' + link + '">' +
+                                '<img class="" style="height: 150px; width:200px; object-fit:cover;"  src="' + standardResolution + '">' +
+                                '</a>' ;
+                               
+                            // AJOUTER NOTRE CODE POUR LA BALISE li DANS LA BALISE ul
+                            baliseUl.innerHTML += codeHtmlLi;
+                        }
+                    }
+                }
+                appelAjax(urlApiAjax, ajouterImage);
+           
